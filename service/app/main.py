7aeec97990580051298cc08a829fe81922cdd409
Adarsh Dubey <dubeyadarshmain@gmail.com>
2025-10-18 00:47:16 +0530

    feat: setup db config

--- service/app/main.py
+++ service/app/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.db import create_db_and_tables
+import logging
 
-app = FastAPI()
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Application is starting up...")
+    create_db_and_tables()
+    yield
+    logger.info("Application is shutting down...")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/health")
